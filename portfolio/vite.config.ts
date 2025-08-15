df973a048c6975fe2510c0b9cb89eb8f5834f652
Justasushant <sushantsharma5128@gmail.com>
2025-08-15 15:47:19 +0530

    Update vite.config.ts

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -4,14 +4,10 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, "client"),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client/src"),
+      "@shared": path.resolve(__dirname, "shared"),
     },
-  },
-  build: {
-    outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: true,
   },
 });
