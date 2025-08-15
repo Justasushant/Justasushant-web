070751c7054471d16130e682a56e79888ae47b18
Justasushant <sushantsharma5128@gmail.com>
2025-08-15 16:15:28 +0530

    Update tailwind.config.ts

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -3,7 +3,9 @@ module.exports = {
   darkMode: ["class"],
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -50,4 +52,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
