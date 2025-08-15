9508529ea3dc4b523f815a4491394cb4c99167a5
Justasushant <sushantsharma5128@gmail.com>
2025-08-15 16:41:23 +0530

    Update tailwind.config.js

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -6,9 +6,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        background: "hsl(var(--background))", // for bg-background
+        foreground: "hsl(var(--foreground))", // optional, if you want text colors
+      },
       borderColor: {
         border: "hsl(var(--border))",
-      }
+      },
     },
   },
   plugins: [],
