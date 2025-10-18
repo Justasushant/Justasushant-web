292f64312db66bf731833354c09f113db5256a06
Sushant <sushantsharma5128@gmail.com>
2025-10-18 10:28:02 +0530

    Add files via upload

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: ["worker-legend-temporary-cia.trycloudflare.com"],
+    allowedHosts: ["varied-attacks-html-ecology.trycloudflare.com"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
